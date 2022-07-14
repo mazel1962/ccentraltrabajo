85ef1c7ddb486c95aebc627f3ca027c08c8005ab
German Salinas <german.salinas@pisquera.cl>
2022-07-14 15:53:03 -0400

    total

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+
+content: ['./public/*.*',
+'./src/*.*',
+'./src/**/*.*',
+'./index.html'],
+theme: {
+  extend: {
+    colors: {
+      plata: '#f2f2f2',
+      suerte: '#37538C',
+      billete: '#027373',
+      moneda:'#5274D9',
+    }
+  },
+
+},
+plugins: [],
+}
